23b32b05ffed1a4dde8219bbee94e246bb80028c
caleosun123 <osunagarciacaleb@gmail.com>
2025-02-21 10:46:14 -0500

    Update main.go

--- api/main.go
+++ api/main.go
@@ -3,12 +3,23 @@ package handler
 import (
   "fmt"
   "net/http"
+  "html/template"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path != "/" {
-    http.NotFound(w, r)
-    fmt.Fprint("404 not found")
-  }
-  fmt.Fprintf(w, "Hello, from go on vercel!")
+func main() {
+  http.HandleFunc("/", HomeHandler)
+  http.HandleFunc("/repeat", RepeatHandler)
+
+  fmt.Println("Server started at port 8080")
+  http.ListenAndServe(":8080", nil)
 }
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method == http.MethodPost {
+    input := r.FormValue("text")
+    data := PageData{Text: input}
+    tmpl := template.Must(template.ParseFiles("static/result.html"))
+    tmpl.Execute(w, data)
+  }
+}
+
