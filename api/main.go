31f20b17684d437202e21d13a66b8563b7fa1c55
caleosun123 <osunagarciacaleb@gmail.com>
2025-02-21 09:58:45 -0500

    Update main.go

--- api/main.go
+++ api/main.go
@@ -6,5 +6,9 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    fmt.Fprint("404 not found")
+  }
   fmt.Fprintf(w, "Hello, from go on vercel!")
 }
